4f636c77a2c8ab278a0f17762a17d39517f4d4c6
Tilman Holube <tilman@holube.de>
2024-03-24 12:27:34 +0100

    add array to module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module de.holube.vac {
     requires static lombok;
     requires org.apache.commons.collections4;
 
+    exports de.holube.vac.array;
     exports de.holube.vac.collections;
     exports de.holube.vac.stream;
 }
