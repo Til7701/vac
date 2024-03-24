1d0a22caa3fc732c9641862d286349942fbdc33a
Tilman Holube <tilman@holube.de>
2024-03-24 11:39:38 +0100

    remove old streams

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module de.holube.vac {
     requires static lombok;
-    requires org.apache.commons.lang3;
     requires org.apache.commons.collections4;
 
     exports de.holube.vac.collections;
     exports de.holube.vac.stream;
-    exports de.holube.vac.stream.list;
 }
