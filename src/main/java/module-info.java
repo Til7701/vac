2d55411d522a9498e57b3171c309cb6b0f5f0027
Tilman Holube <tilman@holube.de>
2024-04-19 16:37:51 +0200

    remove dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,5 @@
 module de.holube.vac {
     requires static lombok;
-    requires org.apache.commons.collections4;
-    requires org.apache.bcel;
-    requires org.antlr.antlr4.runtime;
 
     exports de.holube.vac.array;
     exports de.holube.vac.collections;
